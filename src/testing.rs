32270fb438850270843737f45eac6ebd2859055d
May <MayflowerC10@gmail.com>
2022-10-28 14:14:11 -0400

    Add Ch2 tests

--- src/testing.rs
+++ src/testing.rs
@@ -21,7 +21,7 @@ pub fn test_chapter(chap: &str, file: &str) {
     assert_eq!(expected, actual);
 }
 
-pub fn test_chapter_time(chap: &str, file: &str) {
+pub fn test_chapter_timed(chap: &str, file: &str) {
     let mut s = String::from(".");
     if let Ok(news) = std::env::var("JVM_FOLDER_PATH") {
         s = news;
@@ -45,43 +45,89 @@ pub fn test_chapter_time(chap: &str, file: &str) {
     assert!(time_after_jvm.duration_since(time_before_jvm) < time_after_java.duration_since(time_before_java));
 }
 
-
-#[test]
-fn ch01_hello() {
-    test_chapter("ch01", "Hello")
+mod ch01 {
+    use super::*;
+    #[test]
+    fn hello() {
+        test_chapter("ch01", "Hello")
+    }
+    #[test]
+    fn hello_timed() {
+        test_chapter_timed("ch01", "Hello")
+    } 
+    #[test]
+    fn hello2() {
+        test_chapter("ch01", "Hello2")
+    } 
+    #[test]
+    fn hello2_timed() {
+        test_chapter_timed("ch01", "Hello2")
+    }
+    #[test]
+    fn hello3() {
+        test_chapter("ch01", "Hello3")
+    }
+    #[test]
+    fn hello3_timed() {
+        test_chapter_timed("ch01", "Hello3")
+    } 
+    #[test]
+    fn goodbye() {
+        test_chapter("ch01", "Goodbye")
+    }
+    #[test]
+    fn goodbye_timed() {
+        test_chapter_timed("ch01", "Goodbye")
+    }
 }
-
-#[test]
-fn ch01_hello_time() {
-    test_chapter_time("ch01", "Hello")
-}
-
-#[test]
-fn ch01_hello2() {
-    test_chapter("ch01", "Hello2")
-}
-
-#[test]
-fn ch01_hello2_time() {
-    test_chapter_time("ch01", "Hello2")
-}
-
-#[test]
-fn ch01_hello3() {
-    test_chapter("ch01", "Hello3")
-}
-
-#[test]
-fn ch01_hello3_time() {
-    test_chapter_time("ch01", "Hello3")
-}
-
-#[test]
-fn ch01_goodbye() {
-    test_chapter("ch01", "Goodbye")
-}
-
-#[test]
-fn ch01_goodbye_time() {
-    test_chapter_time("ch01", "Goodbye")
+mod ch02 {
+    use super::*;
+    #[test]
+    fn declare_assign() {
+        test_chapter("ch02", "DeclareAssign")
+    }
+    #[test]
+    fn declare_assign_timed() {
+        test_chapter_timed("ch02", "DeclareAssign")
+    } 
+    #[test]
+    fn floating_point() {
+        test_chapter("ch02", "FloatingPoint")
+    }
+    #[test]
+    fn floating_point_timed() {
+        test_chapter_timed("ch02", "FloatingPoint")
+    } 
+    #[test]
+    fn hello() {
+        test_chapter("ch02", "Hello")
+    }
+    #[test]
+    fn hello_timed() {
+        test_chapter_timed("ch02", "Hello")
+    } 
+    #[test]
+    fn memory_diagram() {
+        test_chapter("ch02", "MemoryDiagram")
+    }
+    #[test]
+    fn memory_diagramtimed() {
+        test_chapter_timed("ch02", "MemoryDiagram")
+    } 
+    #[test]
+    fn printing_vars() {
+        test_chapter("ch02", "PrintingVars")
+    }
+    #[test]
+    fn printing_vars_timed() {
+        test_chapter_timed("ch02", "PrintingVars")
+    } 
+    #[test]
+    fn string_concat() {
+        test_chapter("ch02", "StringConcat")
+    }
+    #[test]
+    fn string_concat_timed() {
+        test_chapter_timed("ch02", "StringConcat")
+    } 
 }

0d977ca9a8413e08b50fe70f3eedc1d9e6f06564
May <MejeC10@gmail.com>
2022-12-27 17:02:12 -0500

    Fix a segfault in speed/Primes
    This patch fixes a segfault introduced by attempting to allocate
    the local variables and operand stack of a frame as a single vector.
    When the program attempted to drop a frame with a non-empty operand
    stack, it convered Values into VarValues, which caused a segfault.
    The solution was to simply empty the operand stack (dropped its elements
    as Values) before deallocating actual vectors.

--- src/frame.rs
+++ src/frame.rs
@@ -5,47 +5,6 @@ use std::vec::Vec;
 use crate::class::{Class, classfile::MethodInfo};
 use crate::reference::object::Object;
 use crate::value::{Value, VarValue};
-/* 
-#[derive(Clone)]
-pub struct DataContainer {
-    pub container: Vec<Value<dyn Class, dyn Object>>,
-}
-
-impl DataContainer {
-    pub fn new(num_locals: usize, stack_size: usize) -> DataContainer {
-        // This is SO INCREDIBLY DANGEROUS but hopefully it's faster
-        // This could be made better by merging Value and VarValue, but for now we just do this
-        let mut container = Vec::with_capacity(num_locals + stack_size);
-        for _ in 0..num_locals {
-            let new_val = unsafe {
-                std::mem::transmute::<VarValue<dyn Class, dyn Object>, 
-                    Value<dyn Class, dyn Object>>(VarValue::Uninit)
-            };
-            container.push(new_val);
-        }
-        DataContainer { container}
-    }
-    pub fn get_local_var(&self, idx: usize) -> VarValue<dyn Class, dyn Object> {
-        unsafe {
-            std::mem::transmute::<Value<dyn Class, dyn Object>, 
-                VarValue<dyn Class, dyn Object>>(self.container[idx].clone())
-        }
-    }
-    pub fn get_local_var_mut(&mut self, idx: usize) -> &mut VarValue<dyn Class, dyn Object> {
-        unsafe {
-            std::mem::transmute::<&mut Value<dyn Class, dyn Object>, 
-                &mut VarValue<dyn Class, dyn Object>>(&mut self.container[idx])
-        }
-    }
-    pub fn set_local_var(&mut self, idx: usize, var: VarValue<dyn Class, dyn Object>) {
-        let new_val = unsafe {
-            std::mem::transmute::<VarValue<dyn Class, dyn Object>, 
-                Value<dyn Class, dyn Object>>(var)
-        };
-        self.container[idx] = new_val;
-    }
-}
-*/
 
 #[derive(Clone)]
 pub struct Frame {
@@ -54,7 +13,6 @@ pub struct Frame {
     pub op_stack: ManuallyDrop<Vec<Value<dyn Class, dyn Object>>>,
     pub rt_const_pool: Rc<dyn Class>, 
     pub current_method: MethodInfo, // It should be fine for this to be a straight-up methodinfo, because methodinfos should be immutable.
-    // This could be made into an Rc at some point, but this *should* work for now.
     pub pc: usize,
 }
 impl Frame {
@@ -71,7 +29,6 @@ impl Frame {
             let p2 = p1.add(num_locals) as *mut Value<dyn Class, dyn Object>;
             (Vec::from_raw_parts(p1, num_locals, num_locals), Vec::from_raw_parts(p2, 0, stack_size))
         };
-
         Frame { local_variables: ManuallyDrop::new(local_variables), op_stack: ManuallyDrop::new(op_stack), 
             rt_const_pool: pool_ref, current_method: method, pc: 0 }
     }
@@ -84,10 +41,12 @@ impl Frame {
 
 impl Drop for Frame {
     fn drop(&mut self) {
+        // Clear to prevent segfaults b/c of misinterpreting Values as VarValues.
+        self.op_stack.clear();
         let _v = unsafe {
             Vec::from_raw_parts(
                 self.local_variables.as_mut_ptr(),
-                self.local_variables.len() + self.op_stack.len(),
+                self.local_variables.len(),
                 self.local_variables.capacity() + self.op_stack.capacity(),
             )
         };
